3251dc687a907931307698a2eff44eb954ed68d5
Krateng <git.noreply@krateng.ch>
2019-03-28 10:53:26 +0100

    Module configuration now correctly redefines default function arguments

--- doreah/persistence.py
+++ doreah/persistence.py
@@ -2,35 +2,35 @@ import pickle
 import os
 
 # set configuration
-# defaultextension	files with this extension will be regarded as valid files. can be overwritten per request.
-# comments			whether files may include commenting (indicated by #)
-# multitab			whether fields can be separated by multiple tabs (this makes empty fields impossible except when trailing)
+# folder	folder to store log files
 def config(folder="storage"):
 	global _folder
 	_folder = folder
 
-# initial config on import, set everything to default
-config()
 
+	global save, load
 
-def save(data,name,folder=_folder):
+	def save(data,name,folder=_folder):
 
-	filename = os.path.join(folder,name + ".gilly")
+		filename = os.path.join(folder,name + ".gilly")
 
-	fl = open(filename,"wb")
-	stream = pickle.dumps(data)
-	fl.write(stream)
-	fl.close()
-
-def load(name,folder=_folder):
-
-	filename = os.path.join(folder,name + ".gilly")
-
-	try:
-		fl = open(filename,"rb")
-		ob = pickle.loads(fl.read())
-	except: ob = None
-	finally:
+		fl = open(filename,"wb")
+		stream = pickle.dumps(data)
+		fl.write(stream)
 		fl.close()
 
-	return ob
+	def load(name,folder=_folder):
+
+		filename = os.path.join(folder,name + ".gilly")
+
+		try:
+			fl = open(filename,"rb")
+			ob = pickle.loads(fl.read())
+		except: ob = None
+		finally:
+			fl.close()
+
+		return ob
+
+# initial config on import, set everything to default
+config()
--- doreah/settings.py
+++ doreah/settings.py
@@ -16,64 +16,117 @@ def config(defaultextension=".ini",files=["settings.ini","settings.conf","config
 	_category = category
 	_onlytext = onlytext
 
-# initial config on import, set everything to default
-config()
+
+	global Settings, get_settings, set_settings, update
+
+
+	# manager object so we can read settings once and retain them
+	class Settings:
+		def __init__(self,**kwargs):
+			self.settings = get_settings(**kwargs)
+
+		def get(self,key):
+			return self.settings.get(key)
 
 
 
 
+	def _interpret(text):
+		if _onlytext: return text
 
-# manager object so we can read settings once and retain them
-class Settings:
-	def __init__(self,**kwargs):
-		self.settings = get_settings(**kwargs)
-
-	def get(self,key):
-		return self.settings.get(key)
-
-
-
-
-def _interpret(text):
-	if _onlytext: return text
-
-	if text.lower() in ["true","yes"]: return True
-	if text.lower() in ["false","no"]: return False
-	if text.startswith("'") and text.endswith("'"): return text[1:-1]
-	if text.startswith('"') and text.endswith('"'): return text[1:-1]
-	try:
-		return int(text)
-	except:
-		pass
-	try:
-		return float(text)
-	except:
-		pass
-	return text
+		if text.lower() in ["true","yes"]: return True
+		if text.lower() in ["false","no"]: return False
+		if text.startswith("'") and text.endswith("'"): return text[1:-1]
+		if text.startswith('"') and text.endswith('"'): return text[1:-1]
+		try:
+			return int(text)
+		except:
+			pass
+		try:
+			return float(text)
+		except:
+			pass
+		return text
 
 
-# get settings
-# keys			list of requested keys. if present, will return list of according values, if not, will return dict of key-value pairs
-# files			which files to parse. later files (higher indicies) will overload earlier ones
-# prefix		only request keys with a certain prefix. key filter will still be applied if present
-# cut_prefix	return keys without the prefix
-# category		return only keys of specific category
-# raw			do not interpret data type, only return strings
-def get_settings(*keys,files=_files,prefix="",cut_prefix=False,category=None,raw=False):
+	# get settings
+	# keys			list of requested keys. if present, will return list of according values, if not, will return dict of key-value pairs
+	# files			which files to parse. later files (higher indicies) will overload earlier ones
+	# prefix		only request keys with a certain prefix. key filter will still be applied if present
+	# cut_prefix	return keys without the prefix
+	# category		return only keys of specific category
+	# raw			do not interpret data type, only return strings
+	def get_settings(*keys,files=_files,prefix="",cut_prefix=False,category=None,raw=False):
 
-	allsettings = {}
+		allsettings = {}
 
-	for f in files:
-		if not os.path.exists(f): continue
+		for f in files:
+			if not os.path.exists(f): continue
 
-		# if category is specified, ignore all entries by default and switch when we encounter the right heading
-		ignore = False if category is None else True
+			# if category is specified, ignore all entries by default and switch when we encounter the right heading
+			ignore = False if category is None else True
 
-		with open(f) as file:
-			lines = file.readlines()
+			with open(f) as file:
+				lines = file.readlines()
 
 
-		for l in lines:
+			for l in lines:
+				# clean up line
+				l = l.replace("\n","")
+				for symbol in _comment:
+					l = l.split(symbol)[0]
+				l = l.strip()
+
+				# check if valid settings entry
+				if l == "": continue
+				if l.startswith(_category[0]):
+					# ignore category headers if we don't care
+					if category is None: continue
+					# otherwise, find out if this is the category we want
+					else:
+						if l.endswith(_category[1]):
+							cat = l[len(_category[0]):-len(_category[1])]
+							ignore = not (cat == category) #if this is the right heading, set ignore to false
+						continue
+
+				if ignore: continue
+
+				if "=" not in l: continue
+
+				# read
+				(key,val) = l.split("=")
+				key = key.strip()
+				val = val.strip() if raw else _interpret(val.strip())
+
+				if key.startswith(prefix):
+					# return keys without the common prefix
+					if cut_prefix:
+						allsettings[key[len(prefix):]] = val
+
+					# return full keys
+					else:
+						allsettings[key] = val
+
+		# no arguments means all settings
+		if len(keys) == 0:
+			return allsettings
+
+		# specific keys requested
+		else:
+			return [allsettings[k] for k in keys]
+
+
+	def set_settings(file,settings):
+
+		if not os.path.exists(file): return
+
+		with open(file,"r") as origfile:
+			lines = origfile.readlines()
+
+		newlines = []
+
+		for origline in lines:
+			l = origline
 			# clean up line
 			l = l.replace("\n","")
 			for symbol in _comment:
@@ -81,102 +134,50 @@ def get_settings(*keys,files=_files,prefix="",cut_prefix=False,category=None,raw
 			l = l.strip()
 
 			# check if valid settings entry
-			if l == "": continue
+			if l == "":
+				newlines.append(origline)
+				continue
 			if l.startswith(_category[0]):
-				# ignore category headers if we don't care
-				if category is None: continue
-				# otherwise, find out if this is the category we want
-				else:
-					if l.endswith(_category[1]):
-						cat = l[len(_category[0]):-len(_category[1])]
-						ignore = not (cat == category) #if this is the right heading, set ignore to false
-					continue
-
-			if ignore: continue
-
-			if "=" not in l: continue
+				newlines.append(origline)
+				continue
+			if "=" not in l:
+				newlines.append(origline)
+				continue
 
 			# read
 			(key,val) = l.split("=")
 			key = key.strip()
-			val = val.strip() if raw else _interpret(val.strip())
-
-			if key.startswith(prefix):
-				# return keys without the common prefix
-				if cut_prefix:
-					allsettings[key[len(prefix):]] = val
-
-				# return full keys
-				else:
-					allsettings[key] = val
-
-	# no arguments means all settings
-	if len(keys) == 0:
-		return allsettings
-
-	# specific keys requested
-	else:
-		return [allsettings[k] for k in keys]
+			val = val.strip()
 
 
-def set_settings(file,settings):
+			if key not in settings:
+				newlines.append(origline)
+				continue
 
-	if not os.path.exists(file): return
+			else:
+				#print("Found key")
+				newline = origline.split("=",1)
+				#print({"linepart":newline[1],"keytoreplace":val,"new":settings[key]})
+				newline[1] = newline[1].replace(val,str(settings[key]))
+				newline = "=".join(newline)
+				newlines.append(newline)
 
-	with open(file,"r") as origfile:
-		lines = origfile.readlines()
-
-	newlines = []
-
-	for origline in lines:
-		l = origline
-		# clean up line
-		l = l.replace("\n","")
-		for symbol in _comment:
-			l = l.split(symbol)[0]
-		l = l.strip()
-
-		# check if valid settings entry
-		if l == "":
-			newlines.append(origline)
-			continue
-		if l.startswith(_category[0]):
-			newlines.append(origline)
-			continue
-		if "=" not in l:
-			newlines.append(origline)
-			continue
-
-		# read
-		(key,val) = l.split("=")
-		key = key.strip()
-		val = val.strip()
+		with open(file,"w") as newfile:
+			newfile.write("".join(newlines))
 
 
-		if key not in settings:
-			newlines.append(origline)
-			continue
 
+
+	# updates a user settings file to a newer format from a default file without overwriting user's settings
+	def update(source="default_settings.ini",target="settings.ini"):
+
+		if not os.path.exists(target):
+			shutil.copyfile(source,target)
 		else:
-			#print("Found key")
-			newline = origline.split("=",1)
-			#print({"linepart":newline[1],"keytoreplace":val,"new":settings[key]})
-			newline[1] = newline[1].replace(val,str(settings[key]))
-			newline = "=".join(newline)
-			newlines.append(newline)
-
-	with open(file,"w") as newfile:
-		newfile.write("".join(newlines))
+			usersettings = get_settings(files=[target],raw=True)
+			shutil.copyfile(source,target)
+			set_settings(target,usersettings)
 
 
-
-
-# updates a user settings file to a newer format from a default file without overwriting user's settings
-def update(source="default_settings.ini",target="settings.ini"):
-
-	if not os.path.exists(target):
-		shutil.copyfile(source,target)
-	else:
-		usersettings = get_settings(files=[target],raw=True)
-		shutil.copyfile(source,target)
-		set_settings(target,usersettings)
+# initial config on import, set everything to default
+config()
