c31a2433fdeee3bea7bc55a7d8f432c44e73ecc6
Krateng <git.noreply@krateng.ch>
2019-03-29 16:20:48 +0100

    Reverted previous design decision because I'm young and impulsive (and it messed up direct import of functions)

--- doreah/logging.py
+++ doreah/logging.py
@@ -19,93 +19,85 @@ def config(logfolder="logs",timeformat="%Y/%m/%d %H:%M:%S",defaultmodule="main",
 	_verbosity = verbosity
 	_wait = wait
 
-
-
-	global log, flush, logh1, logh2
-
-
-
-	# Log entry
-	# module		allows discrimination between modules of a program. Will be prepended in console output and will determine the separate file for disk storage
-	# 				defaults to actual name of the calling module or "main" for the main script
-	# header		determines the hierarchical position of the entry.
-	# indent		adds indent to the log entry
-	# importance	low means important. if higher than the configured verbosity, entry will not be shown on console
-	def log(*msgs,module=None,header=None,indent=0,importance=0):
-
-		now = datetime.datetime.utcnow().strftime(_timeformat)
-
-		# log() can be used to add empty line
-		if len(msgs) == 0: msgs = ("",)
-
-		# make it easier to log data structures and such
-		msgs = tuple([str(msg) for msg in msgs])
-
-		# header formating
-		if header == 2:
-			msgs = ("","","####") + msgs + ("####","")
-		elif header == 1:
-			msgs = ("","","","# # # # #","") + msgs + ("","# # # # #","","")
-
-		# indent
-		prefix = "\t" * indent
-
-		# module name
-		if module is None:
-			try:
-				module = inspect.getmodule(inspect.stack()[1][0]).__name__
-				if module == "__main__": module = _defaultmodule
-			except:
-				module = "interpreter"
-
-		global _wait, _queue
-		if _wait:
-			for msg in msgs:
-				_queue.append({"time":now,"prefix":prefix,"msg":msg,"module":module,"console":(importance <= _verbosity)})
-		else:
-			# console output
-			if (importance <= _verbosity):
-				for msg in msgs:
-					print("[" + module + "] " + prefix + msg)
-
-			# file output
-			logfilename = _logfolder + "/" + module + ".log"
-			os.makedirs(os.path.dirname(logfilename), exist_ok=True)
-			with open(logfilename,"a") as logfile:
-				for msg in msgs:
-					logfile.write(now + "  " + prefix + msg + "\n")
-
-
-	def flush():
-		global _queue
-		for entry in _queue:
-			# console output
-			if entry["console"]:
-				print("[" + entry["module"] + "] " + entry["prefix"] + entry["msg"])
-
-			# file output
-			logfilename = _logfolder + "/" + entry["module"] + ".log"
-			os.makedirs(os.path.dirname(logfilename), exist_ok=True)
-			with open(logfilename,"a") as logfile:
-				logfile.write(entry["time"] + "  " + entry["prefix"] + entry["msg"] + "\n")
-
-		_queue = []
-
-	# Quicker way to add header
-	def logh1(*args,**kwargs):
-		return log(*args,**kwargs,header=1)
-	def logh2(*args,**kwargs):
-		return log(*args,**kwargs,header=2)
-
-
-
-
-
-
-
 	# things to do on reconfiguration
 	if not wait: flush()
 
 
 # initial config on import, set everything to default
 config()
+
+
+
+
+
+# Log entry
+# module		allows discrimination between modules of a program. Will be prepended in console output and will determine the separate file for disk storage
+# 				defaults to actual name of the calling module or "main" for the main script
+# header		determines the hierarchical position of the entry.
+# indent		adds indent to the log entry
+# importance	low means important. if higher than the configured verbosity, entry will not be shown on console
+def log(*msgs,module=None,header=None,indent=0,importance=0):
+
+	now = datetime.datetime.utcnow().strftime(_timeformat)
+
+	# log() can be used to add empty line
+	if len(msgs) == 0: msgs = ("",)
+
+	# make it easier to log data structures and such
+	msgs = tuple([str(msg) for msg in msgs])
+
+	# header formating
+	if header == 2:
+		msgs = ("","","####") + msgs + ("####","")
+	elif header == 1:
+		msgs = ("","","","# # # # #","") + msgs + ("","# # # # #","","")
+
+	# indent
+	prefix = "\t" * indent
+
+	# module name
+	if module is None:
+		try:
+			module = inspect.getmodule(inspect.stack()[1][0]).__name__
+			if module == "__main__": module = _defaultmodule
+		except:
+			module = "interpreter"
+
+	global _wait, _queue
+	if _wait:
+		for msg in msgs:
+			_queue.append({"time":now,"prefix":prefix,"msg":msg,"module":module,"console":(importance <= _verbosity)})
+	else:
+		# console output
+		if (importance <= _verbosity):
+			for msg in msgs:
+				print("[" + module + "] " + prefix + msg)
+
+		# file output
+		logfilename = _logfolder + "/" + module + ".log"
+		os.makedirs(os.path.dirname(logfilename), exist_ok=True)
+		with open(logfilename,"a") as logfile:
+			for msg in msgs:
+				logfile.write(now + "  " + prefix + msg + "\n")
+
+
+def flush():
+	global _queue
+	for entry in _queue:
+		# console output
+		if entry["console"]:
+			print("[" + entry["module"] + "] " + entry["prefix"] + entry["msg"])
+
+		# file output
+		logfilename = _logfolder + "/" + entry["module"] + ".log"
+		os.makedirs(os.path.dirname(logfilename), exist_ok=True)
+		with open(logfilename,"a") as logfile:
+			logfile.write(entry["time"] + "  " + entry["prefix"] + entry["msg"] + "\n")
+
+	_queue = []
+
+# Quicker way to add header
+def logh1(*args,**kwargs):
+	return log(*args,**kwargs,header=1)
+def logh2(*args,**kwargs):
+	return log(*args,**kwargs,header=2)
--- doreah/persistence.py
+++ doreah/persistence.py
@@ -8,29 +8,27 @@ def config(folder="storage"):
 	_folder = folder
 
 
-	global save, load
-
-	def save(data,name,folder=_folder):
-
-		filename = os.path.join(folder,name + ".gilly")
-
-		fl = open(filename,"wb")
-		stream = pickle.dumps(data)
-		fl.write(stream)
-		fl.close()
-
-	def load(name,folder=_folder):
-
-		filename = os.path.join(folder,name + ".gilly")
-
-		try:
-			fl = open(filename,"rb")
-			ob = pickle.loads(fl.read())
-		except: ob = None
-		finally:
-			fl.close()
-
-		return ob
-
 # initial config on import, set everything to default
 config()
+
+def save(data,name,folder=_folder):
+
+	filename = os.path.join(folder,name + ".gilly")
+
+	fl = open(filename,"wb")
+	stream = pickle.dumps(data)
+	fl.write(stream)
+	fl.close()
+
+def load(name,folder=_folder):
+
+	filename = os.path.join(folder,name + ".gilly")
+
+	try:
+		fl = open(filename,"rb")
+		ob = pickle.loads(fl.read())
+	except: ob = None
+	finally:
+		fl.close()
+
+	return ob
