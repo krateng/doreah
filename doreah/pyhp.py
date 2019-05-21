6257f0f111ee9b5551163549525c87c85e23c62b
Krateng <git.noreply@krateng.ch>
2019-05-21 14:17:47 +0200

    Added include statement to pyhp

--- doreah/pyhp.py
+++ doreah/pyhp.py
@@ -1,6 +1,7 @@
 from lxml import etree
 from copy import deepcopy
 import re
+import os
 
 from ._internal import DEFAULT, defaultarguments, doreahconfig
 
@@ -23,7 +24,7 @@ config()
 
 
 @defaultarguments(_config,interpret="interpret")
-def file(path,d,interpret=DEFAULT):
+def file(path,d,interpret=DEFAULT,noroot=False):
 	"""Parses a pyhp source file and returns the generated html code.
 
 	:param string path: Location of the pyhp source file
@@ -35,11 +36,23 @@ def file(path,d,interpret=DEFAULT):
 	with open(path,"r") as f:
 		content = f.read()
 
-	return parse(content,d,interpret=interpret)
+	directory = os.path.dirname(os.path.abspath(path))
+
+	return parse(content,d,interpret=interpret,directory=directory,noroot=noroot)
+
+
+def _file(path,d,interpret=DEFAULT,noroot=False):
+
+	with open(path,"r") as f:
+		content = f.read()
+
+	directory = os.path.dirname(os.path.abspath(path))
+
+	return _parse(content,d,interpret=interpret,directory=directory,noroot=noroot)
 
 
 @defaultarguments(_config,interpret="interpret")
-def parse(src,d,interpret=DEFAULT):
+def parse(src,d,interpret=DEFAULT,directory=None,noroot=False):
 	"""Parses pyhp source and returns the generated html code.
 
 	:param string src: Source string
@@ -47,30 +60,73 @@ def parse(src,d,interpret=DEFAULT):
 	:param function interpret: Function that determines string representations of variables
 	:return: HTML source
 	"""
-	doc = etree.XML(src)
 
-	doc = _parse_node(doc,d,interpret)[0]
+	doc = _parse(src,d,interpret=interpret,directory=directory,noroot=noroot)
 
-	return etree.tostring(doc).decode().replace("&gt;",">").replace("&lt;","<")
+
+	#raw = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+	raw = etree.tostring(doc,encoding="unicode",method="html").replace("&gt;",">").replace("&lt;","<")
+
+
+	return raw
 	# yes I know
 	# it's a temporary thing okay
 
+# to tree
+def _parse(src,d,interpret=DEFAULT,directory=None,noroot=False):
 
-def _parse_node(node,d,interpret):
+	if noroot:
+		# pseudo root
+		src = "<pseudoroot>" + src + "</pseudoroot>"
 
+	doc = etree.XML(src)
+	doc = _parse_node(doc,d,interpret,directory=directory)[0]
+
+	if noroot:
+		return list(doc) #all child elements
+
+	return doc
+
+
+def _parse_node(node,d,interpret,directory=None):
+
+
+	## fix
+	#if node.tag == "html":
+	#	node.attrib["xmlns"] = "http://www.w3.org/1999/xhtml"
 
 
 	## parse pyhp nodes
 
 	if node.tag == "pyhp":
 
+		#### INCLUDE
+
+		if _attr(node,"include") is not None:
+			filename = _attr(node,"include")
+			if directory is None:
+				# relative to execution path
+				filename = filename
+			else:
+				# relative to this file
+				filename = os.path.join(directory,filename)
+
+			#with open(filename,"r") as f:
+			#	raw = f.read()
+
+			subnodes = _file(filename,d,interpret=interpret,noroot=True)
+			print(subnodes)
+
+			return subnodes + [node.tail]
+
+
 		#### IF
 
-		if _attr(node,"if") is not None:
+		elif _attr(node,"if") is not None:
 			if eval(_attr(node,"if"),d):
 				nodestoreturn = [node.text]
 				for sn in node:
-					nodestoreturn += _parse_node(sn,d,interpret)
+					nodestoreturn += _parse_node(sn,d,interpret,directory=directory)
 				nodestoreturn += [node.tail]
 				return nodestoreturn
 			else:
@@ -95,7 +151,7 @@ def _parse_node(node,d,interpret):
 
 					# parse with the normal dict and the current element of the loop
 					sn = deepcopy(sn)
-					nodestoreturn += _parse_node(sn,{**d, **{_attr(node,"for"):element}},interpret)
+					nodestoreturn += _parse_node(sn,{**d, **{_attr(node,"for"):element}},interpret,directory=directory)
 
 
 
@@ -136,10 +192,11 @@ def _parse_node(node,d,interpret):
 		newsubnodes = []
 		for subnode in subnodes:
 			node.remove(subnode)
-			newsubnodes += _parse_node(subnode,d,interpret)
+			newsubnodes += _parse_node(subnode,d,interpret,directory=directory)
 
 		prev = None
 		for nsn in newsubnodes:
+
 
 			if nsn is None: continue
 
@@ -148,11 +205,13 @@ def _parse_node(node,d,interpret):
 				# string nodes need to be either the tail of their previous node, or if they're
 				# in first position, the text of the parent node
 				if prev is None:
+					#if node.tag == "head": print(str(nsn) + " appended as text to " + str(node))
 					try:
 						node.text += nsn
 					except:
 						node.text = nsn
 				else:
+					#if node.tag == "head": print(str(nsn) + " appended as tail to " + str(prev))
 					try:
 						prev.tail += nsn
 					except:
@@ -162,9 +221,15 @@ def _parse_node(node,d,interpret):
 				prev = nsn
 
 
+
 		for nsn in newsubnodes:
-			if not isinstance(nsn,str) and nsn is not None:
+			if isinstance(nsn,etree._Element):
 				node.append(nsn)
+
+		if node.tag == "head":
+			#print(newsubnodes)
+
+			print(etree.tostring(node))
 
 		return [node]
 
