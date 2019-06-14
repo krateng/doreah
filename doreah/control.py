a5b6f38b62a05bef2eed4b4a4804536187c60a80
Krateng <git.noreply@krateng.ch>
2019-06-14 18:39:25 +0200

    Worked on control module

--- doreah/control.py
+++ doreah/control.py
@@ -4,27 +4,46 @@ import os
 import urllib.request
 import shutil
 import distutils.dir_util
+import sys
 
 config = DoreahConfig("control")
 
 
 class Controller:
-	def __init__(self,procname,source_url,source_path,custom_functions={}):
-		self.source_url = source_url
-		self.source_path = source_path
+	def __init__(self,procname,github_project,github_path,custom_functions={},main_script="server.py"):
+		self.source_url = "https://github.com/" + github_project
+		self.name = github_project.split("/")[-1]
+		self.source_path = github_path
 		self.custom_functions = custom_functions
+		self.main_script = main_script
 
 	def evoke(self):
 		if sys.argv[1] == "start": self._start()
 		elif sys.argv[1] == "restart": self._start()
 		elif sys.argv[1] == "stop": self._stop()
 		elif sys.argv[1] == "update": self._update()
-		elif sys.argv[1] == "install": installhere()
+		elif sys.argv[1] == "install": _install()
+		elif sys.argv[1] in self.custom_functions:
+			try:
+				self.custom_functions[sys.argv[1]](sys.argv[2:])
+			except:
+				print("Invalid arguments.")
+
 		else: print("Valid commands:","start","restart","stop","update","install",*[k for k in self.custom_functions])
+
+	def _gotodir(self):
+		if os.path.exists(self.main_script):
+			return True
+		elif os.path.exists("/opt/" + self.name + "/" + self.main_script):
+			os.chdir("/opt/" + self.name)
+			return True
+
+		print("Installation could not be found.")
+		return False
 
 	def _update(self):
 
-		if not gotodir(): return False
+		if not self._gotodir(): return False
 
 		if os.path.exists("./.dev"):
 			print("Better not overwrite the development server!")
@@ -32,7 +51,7 @@ class Controller:
 
 		print("Updating...")
 
-		os.system("wget " + self.source_url)
+		os.system("wget " + self.source_url + "/archive/master.zip")
 		with zipfile.ZipFile("./master.zip","r") as z:
 
 
@@ -48,13 +67,11 @@ class Controller:
 		os.remove("./master.zip")
 
 
-		distutils.dir_util.copy_tree("./maloja-master/","./",verbose=2)
-		shutil.rmtree("./maloja-master")
+		distutils.dir_util.copy_tree("./" + self.name + "-master/","./",verbose=2)
+		shutil.rmtree("./" + self.name + "-master")
 		print("Done!")
 
-		os.chmod("./maloja",os.stat("./maloja").st_mode | stat.S_IXUSR)
-
-		print("Make sure to update required modules! (" + yellow("pip3 install -r requirements.txt --upgrade --no-cache-dir") + ")")
+		os.chmod(self.name,os.stat(self.name).st_mode | stat.S_IXUSR)
 
 		if stop(): start() #stop returns whether it was running before, in which case we restart it
 
