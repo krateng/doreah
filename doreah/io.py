790f20baaf30dd8477cab01a6c2f21c3d6a0dfc1
Krateng <git.noreply@krateng.ch>
2019-10-10 17:20:30 +0200

    Added progress bar

--- doreah/io.py
+++ doreah/io.py
@@ -38,3 +38,93 @@ def ask(msg,default=True,repeat=False):
 			return ask(msg,default,repeat)
 		else:
 			return None
+
+
+def ellipsis(txt,length,padded=False):
+	if len(txt) > length: return txt[:length-3] + "..."
+	else:
+		if padded:
+			while len(txt) < length: txt += " "
+		return txt
+
+
+# credit to StackOverflow user Greenstick
+def print_progress_bar(num=None,prct=None,prefix="",suffix="",decimals=0,length=100,fill="█",manualend=False):
+	if prct is None: prct = num[0] / num[1]
+	percent = ("{0:." + str(decimals) + "f}").format(100 * prct)
+	filledLength = int(length * prct)
+	bar = fill * filledLength + '-' * (length - filledLength)
+	print('\r%s |%s| %s%% %s' % (prefix, bar, percent, ellipsis(suffix,50,padded=True)), end = '\r')
+	#print(num)
+	# Print New Line on Complete
+	if prct == 1 and not manualend:
+		print()
+
+
+class ProgressBar:
+	def __init__(self,max,prefix="",decimals=0,length=100,fill="█"):
+		self.max = max
+		self.prefix = prefix
+		self.decimals = decimals
+		self.length = length
+		self.fill = fill
+		self.current = 0
+		self.step = ""
+		self.finished = False
+
+	def prnt(self,prct=None,num=None,manualend=False):
+		print_progress_bar(prct=prct,num=num,prefix=self.prefix,suffix=self.step,decimals=self.decimals,length=self.length,fill=self.fill,manualend=manualend)
+
+	def print(self):
+		self.prnt(num=(self.current,self.max))
+
+	def progress(self,num=1,suffix=""):
+		self.current += num
+		self.step = suffix
+		self.print()
+		if self.current == self.max: self.finished = True
+
+	def done(self):
+		self.current = self.max
+		if not self.finished:
+		#self.step = "Done!"
+			self.print()
+			self.finished = True
+
+class NestedProgressBar(ProgressBar):
+	def __init__(self,max,prefix="",decimals=0,length=100,fill="█",manual=False):
+		self.layers = [[0,max]]
+		self.prefix = prefix
+		self.decimals = decimals
+		self.length = length
+		self.fill = fill
+		self.step = ""
+		self.manual = manual
+
+	def print(self):
+		prct = 0
+		layervalue = 1
+		for l in self.layers:
+			part = layervalue / l[1] # how much of the bar is at stake per unit of this layer
+			prct += part * l[0] # so much needs to be added because of current progress
+			layervalue = part # one part will be analyzed on the next layer
+		self.prnt(prct=prct,manualend=True)
+		#print(self.layers,self.step)
+		# Print New Line on Complete
+		if self.layers[0][0] == self.layers[0][1]:
+			print()
+
+	def godeeper(self,max):
+		self.layers.append([0,max])
+		if max == 0: self.layers[-1] = [1,1] # already done because empty
+
+	def done(self):
+		self.layers.pop()
+		self.progress()
+
+
+	def progress(self,num=1,step=""):
+		self.step = step
+		self.layers[-1][0] += num
+		self.print()
+		if self.layers[-1][0] == self.layers[-1][1] and not self.manual: self.done()
