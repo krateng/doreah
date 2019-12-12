6bb1eee4e6d2d9d161866c06f329841dde3f1c6c
Krateng <git.noreply@krateng.ch>
2019-12-12 17:22:16 +0100

    Updated Controller

--- doreah/control.py
+++ doreah/control.py
@@ -5,6 +5,8 @@ import urllib.request
 import shutil
 import distutils.dir_util
 import sys
+import subprocess
+import signal
 #import inspect
 
 config = DoreahConfig("control")
@@ -52,71 +54,47 @@ def mainfunction(shortcuts,shield=False):
 
 
 class Controller:
-	def __init__(self,procname,github_project,github_path,custom_functions={},main_script="server.py"):
-		self.source_url = "https://github.com/" + github_project
-		self.name = github_project.split("/")[-1]
-		self.source_path = github_path
-		self.custom_functions = custom_functions
-		self.main_script = main_script
+	def __init__(self,pkgname,processname=None,prettyname=None):
+		self.pkgname = pkgname
+		self.processname = processname if processname is not None else self.pkgname
+		self.prettyname = prettyname if prettyname is not None else self.processname.capitalize()
 
-	def evoke(self):
-		if sys.argv[1] == "start": self._start()
-		elif sys.argv[1] == "restart": self._start()
-		elif sys.argv[1] == "stop": self._stop()
-		elif sys.argv[1] == "update": self._update()
-		elif sys.argv[1] == "install": _install()
-		elif sys.argv[1] in self.custom_functions:
-			try:
-				self.custom_functions[sys.argv[1]](sys.argv[2:])
-			except:
-				print("Invalid arguments.")
+		self.actions = {
+			"start":self.start,
+			"restart":self.restart,
+			"stop":self.stop
+		}
 
-		else: print("Valid commands:","start","restart","stop","update","install",*[k for k in self.custom_functions])
 
-	def _gotodir(self):
-		if os.path.exists(self.main_script):
+	def getInstance(self):
+		try:
+			output = subprocess.check_output(["pidof",self.processname])
+			pid = int(output)
+			return pid
+		except:
+			return None
+	def is_running(self):
+		return getInstance() is not None
+
+	def start(self):
+		try:
+			p = subprocess.Popen(["python3","-m",self.pkgname + ".main"],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+			print(col["green"](self.prettyname + " started!") + " PID: " + str(p.pid))
+		except:
+			print("Error while starting " + self.prettyname + ".")
+			return False
+
+	def restart(self):
+		wasrunning = stop()
+		start()
+		return wasrunning
+
+	def stop(self):
+		pid = getInstance()
+		if pid is None:
+			print(self.prettyname + " is not running")
+			return False
+		else:
+			os.kill(pid,signal.SIGTERM)
+			print(self.prettyname + " stopped! PID: " + str(pid))
 			return True
-		elif os.path.exists("/opt/" + self.name + "/" + self.main_script):
-			os.chdir("/opt/" + self.name)
-			return True
-
-		print("Installation could not be found.")
-		return False
-
-	def _update(self):
-
-		if not self._gotodir(): return False
-
-		if os.path.exists("./.dev"):
-			print("Better not overwrite the development server!")
-			return
-
-		print("Updating...")
-
-		os.system("wget " + self.source_url + "/archive/master.zip")
-		with zipfile.ZipFile("./master.zip","r") as z:
-
-
-			for f in z.namelist():
-				if f.startswith(self.source_path):
-					z.extract(f)
-			for dir,_,files in os.walk("./" + self.source_path):
-				for f in files:
-					origfile = os.path.join(dir,f)
-					newfile = os.path.join(dir[(len(self.source_path)+1):],f)
-					os.renames(origfile,newfile) #also prunes empty directory
-
-		os.remove("./master.zip")
-
-
-		distutils.dir_util.copy_tree("./" + self.name + "-master/","./",verbose=2)
-		shutil.rmtree("./" + self.name + "-master")
-		print("Done!")
-
-		os.chmod(self.name,os.stat(self.name).st_mode | stat.S_IXUSR)
-
-		if stop(): start() #stop returns whether it was running before, in which case we restart it
-
-
-	def generate_bash_completion(self):
-		pass
