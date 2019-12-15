ddebd3ae53982e50a270c35a31608e2c3de7d1a8
Krateng <git.noreply@krateng.ch>
2019-12-15 15:56:21 +0100

    Renaming

--- doreah/packageutils.py
+++ doreah/packageutils.py
@@ -12,20 +12,20 @@ config = DoreahConfig("packageutils",
 #if config["packagename"] is None: config(packagename=)
 
 try:
-	_DATA_DIR = os.environ["XDG_DATA_HOME"].split(":")[0]
-	assert os.path.exists(_DATA_DIR)
+	HOME_DIR = os.environ["XDG_DATA_HOME"].split(":")[0]
+	assert os.path.exists(HOME_DIR)
 except:
-	_DATA_DIR = os.path.join(os.environ["HOME"],".local/share/")
+	HOME_DIR = os.path.join(os.environ["HOME"],".local/share/")
 
 DATA_DIR = None # the actual folder for this package
 
 def _set_datafolder():
 	global DATA_DIR
 	if DATA_DIR is None:
-		DATA_DIR = os.path.join(_DATA_DIR,config["packagename"])
+		DATA_DIR = os.path.join(HOME_DIR,config["packagename"])
 		os.makedirs(DATA_DIR,exist_ok=True)
 
-def datafolder(*args):
+def pkgdata(*args):
 	_set_datafolder()
 	return os.path.join(DATA_DIR,*args)
 
