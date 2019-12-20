1859eb0f1502c891d823e31e8fa9d6cecaba177d
Krateng <git.noreply@krateng.ch>
2019-12-20 15:31:29 +0100

    PYHP3 experimenting

--- doreah/pyhp.py
+++ doreah/pyhp.py
@@ -5,9 +5,13 @@ config = DoreahConfig("pyhp",
 	version=2
 )
 
+
 if config["version"] == 1:
 	print("Using PYHP version 1")
 	from .pyhp1 import *
 elif config["version"] == 2:
 	print("Using PYHP version 2")
 	from .pyhp2 import *
+elif config["version"] == 3:
+	print("Using PYHP version 3")
+	from .pyhp3 import *
