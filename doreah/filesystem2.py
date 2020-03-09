f7ec182cf8a194ab8165def7e9653f4de248ba86
Krateng <git.noreply@krateng.ch>
2020-03-09 22:51:14 +0100

    Started working on more organized filesystem module

---
+++ doreah/filesystem2.py
@@ -0,0 +1,124 @@
+from ._internal import DEFAULT, defaultarguments, DoreahConfig
+
+config = DoreahConfig("filesystem2",
+	ignore_dotfiles=True
+)
+
+
+import os
+import shutil
+import math
+
+class FileWrapper:
+	pass
+
+
+# ABC
+
+class AbstractPath:
+	def parent(self):
+		try:
+			parenttype = self.IS_IN
+		except:
+			parenttype = self.__class__
+		return parenttype(self.path[:-1])
+
+
+class AbstractDirectory(AbstractPath):
+	def mount(self,pth):
+		#return self.__class__(self.path + pth.path)
+		res = combines[(type(self),type(pth))]
+		return res(self.path + pth.path)
+
+	def join(self,pth):
+		return os.path.join(self.__path__(),pth)
+
+class AbstractFile(AbstractPath):
+	pass
+
+
+
+# ABSOLUTE
+
+class AbsolutePath(AbstractPath):
+	def __init__(self,path):
+		if isinstance(path,str):
+			path = os.path.abspath(path)
+			path = path.strip("/")
+			self.path = path.split("/")
+		else:
+			self.path = path
+
+	def __path__(self):
+		return os.path.join("/",*self.path)
+
+
+class AbsoluteDirectory(AbstractDirectory,AbsolutePath):
+	def __getitem__(self,node):
+		trgt = self.join(node)
+		if os.path.exists(trgt):
+			return AbsoluteFile(trgt)
+		else:
+			os.makedirs(trgt,exist_ok=True)
+			return AbsoluteDirectory(trgt)
+
+	def _listdir(self):
+		return os.listdir(self.__path__())
+
+	def files(self):
+		for e in self._listdir():
+			if os.path.isfile(self.join(e)):
+				yield RelativeFile(e)
+
+	def folders(self):
+		for e in self._listdir():
+			if not os.path.isfile(self.join(e)):
+				yield RelativeDirectory(e)
+
+	def all_files_relative(self,maxdepth=math.inf):
+		if maxdepth == 0: return
+		for f in self.files():
+			yield f
+		for f in self.folders():
+			for ff in self.mount(f).all_files_relative(maxdepth=maxdepth-1):
+				yield f.mount(ff)
+
+
+class AbsoluteFile(AbstractFile,AbsolutePath):
+	IS_IN = AbsoluteDirectory
+	pass
+
+
+# RELATIVE
+
+class RelativePath(AbstractPath):
+	def __init__(self,path):
+		if isinstance(path,str):
+			path = path.strip("/")
+			self.path = path.split("/")
+		else:
+			self.path = path
+
+
+	def mountin(self,pth):
+		return pth.mount(self)
+
+	def __path__(self):
+		return os.path.join(*self.path)
+
+class RelativeDirectory(AbstractDirectory,RelativePath):
+	pass
+
+class RelativeFile(AbstractFile,RelativePath):
+	IS_IN = RelativeDirectory
+	pass
+
+
+
+combines = {
+	(AbsoluteDirectory,RelativeDirectory): AbsoluteDirectory,
+	(AbsoluteDirectory,RelativeFile): AbsoluteFile,
+	(RelativeDirectory,RelativeDirectory): RelativeDirectory,
+	(RelativeDirectory,RelativeFile): RelativeFile
+
+}
