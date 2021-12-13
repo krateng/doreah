f9ccaa92530777f7220f097c531af32a51315a72
krateng <git.noreply@krateng.ch>
2021-12-13 13:35:26 +0100

    First working prototype of new configuration module

--- doreah/configuration.py
+++ doreah/configuration.py
@@ -16,73 +16,98 @@ JINJAENV = Environment(
 
 class Configuration:
 
-	def __init__(self,settings,configfiles=("settings.ini",)):
+
+	def __init__(self,settings,configfile="settings.ini",save_endpoint="/settings"):
 		flatten = {key.lower():settings[category][key] for category in settings for key in settings[category]}
 		self.categories = {cat:[k for k in settings[cat]] for cat in settings}
 		self.types = {k:flatten[k][0] for k in flatten}
 		self.names = {k:flatten[k][1] for k in flatten}
 		self.defaults = {k:flatten[k][2] for k in flatten}
-		self.expire = {k:flatten[k][3] if len(flatten[k])>3 else -1 for k in flatten}
+		self.usersettings = {}
+		#self.expire = {k:flatten[k][3] if len(flatten[k])>3 else -1 for k in flatten}
 
-		self.configfiles = configfiles
+		self.configfile = configfile
+		self.save_endpoint = save_endpoint
 
-		self.user_overrides = {}
-		self.load_from_files()
+		self.load_from_file()
 
+	def update(self,settings):
+		for k in settings:
+			settings[k] = self.types[k].sanitize(settings[k])
+		self.usersettings.update(settings)
+		self.write_to_file()
 
-
+	def get_user(self,key):
+		return self.usersettings.get(key)
+	def get_default(self,key):
+		return self.defaults.get(key)
+	def get_active(self,key):
+		return self.get_user(key) or self.get_default(key)
 
 	def __getitem__(self,key):
-		if key in self.user_overrides: return self.user_overrides[key]
-		if key in self.defaults: return self.defaults[key]
-		return None
+		return self.get_active(key)
+		#if key in self.usersettings: return self.usersettings[key]
+		#if key in self.defaults: return self.defaults[key]
+		#return None
 
 	def __iter__(self):
 		return (k for k in self.defaults)
 
 	def todict(self):
 		d = {k:self.defaults[k] for k in self.defaults}
-		d.update(self.user_overrides)
+		d.update(self.usersettings)
 		return d
 
 	def html(self):
 		template = JINJAENV.get_template("settings.jinja")
 		return template.render({"configuration":self})
 
-	def load_from_files(self):
-		for f in self.configfiles:
-			settings = self.load_from_file(f)
-			self.user_overrides.update(settings)
-
-
-	def load_from_file(self,filename):
-		ext = filename.split(".")[-1].lower()
+	def load_from_file(self):
+		ext = self.configfile.split(".")[-1].lower()
 		try:
-			settings = formats.loaders[ext].load_file(filename)
+			settings = formats.loaders[ext].load_file(self.configfile)
 			settings = {k.lower():settings[k] for k in settings}
 			settings = {
 				k:settings[k] for k in settings if
 				k in self.types and self.types[k].validate(settings[k])
 			}
-			return settings
+			self.usersettings = settings
 		except:
-			print("Could not load file",filename)
-			return {}
+			print("Could not load file",self.configfile)
 
+	def write_to_file(self):
+		ext = self.configfile.split(".")[-1].lower()
+		try:
+			formats.loaders[ext].write_file(self.configfile,self.usersettings)
+		except:
+			print("Could not write file",self.configfile)
 
 # this is just a namespace, not a real class
 class types:
 
 	class SettingType:
-		def html(self,default,user):
+		def __init__(self):
+			pass
+
+		def html(self,active,default,user,setting):
 			template = JINJAENV.get_template(self.jinjafile())
-			return template.render({"type":self,"default":default,"user":user})
+			if active is None: active = ""
+			if default is None: default = ""
+			if user is None: user = ""
+			return template.render({"type":self,"active":active,"default":default,"user":user,"setting":setting})
 
 		def jinjafile(self):
 			return "types/" + self.__class__.__name__ + ".jinja"
 
+		def sanitize(self,input):
+			if self.validate(input):
+				return input
+			else:
+				return self.default
+
 	class Integer(SettingType):
 		regex = "[0-9]+"
+		default = 0
 
 		def __init__(self,min=-math.inf,max=math.inf):
 			self.min = min
@@ -93,6 +118,7 @@ class types:
 
 	class String(SettingType):
 		regex = "([^'\"]*)"
+		default = ""
 
 		def __init__(self,minlength=0,maxlength=math.inf):
 			self.minlength = minlength
@@ -114,7 +140,13 @@ class types:
 		def validate(self,input):
 			return input in self.options
 
+	class MultiChoice(Choice):
+		def validate(self,input):
+			return all([i in self.options for i in input])
+
 	class Set(SettingType):
+		default = []
+
 		def __init__(self,type,minmembers=0,maxmembers=math.inf):
 			self.type = type
 			self.minmembers = minmembers
@@ -123,16 +155,42 @@ class types:
 		def validate(self,input):
 			return len(input) <= self.maxmembers and len(input) >= self.minmembers and all(self.type.validate(m) for m in input)
 
+	class List(SettingType):
+		default = []
+
+		def __init__(self,type,minmembers=0,maxmembers=math.inf):
+			self.type = type
+			self.minmembers = minmembers
+			self.maxmembers = maxmembers
+
+		def validate(self,input):
+			return len(input) <= self.maxmembers and len(input) >= self.minmembers and all(self.type.validate(m) for m in input)
+
+	class Boolean(SettingType):
+		def validate(self,input):
+			return True
+
 
 class formats:
 	class GenericHandler:
+
 		def load_file(self,filename):
 			with open(filename) as descriptor:
 				return self.load_descriptor(descriptor)
 
+		def write_file(self,filename,data):
+			with open(filename,"w") as descriptor:
+				return self.write_descriptor(descriptor,data)
+
+
 		def load_descriptor(self,descriptor):
 			txt = descriptor.read()
-			return self.load_text(txt)
+			return self.text_to_data(txt)
+
+		def write_descriptor(self,descriptor,data):
+			txt = self.data_to_text(data)
+			return descriptor.write(txt)
+
 
 	class YAMLHandler(GenericHandler):
 		import yaml
@@ -164,7 +222,7 @@ class formats:
 		]))
 
 
-		def load_text(self,txt):
+		def text_to_data(self,txt):
 			result = {}
 			for l in txt.split("\n"):
 				match = self.regex_fullline.fullmatch(l)
@@ -179,6 +237,14 @@ class formats:
 
 			return result
 
+		def data_to_text(self,data):
+			lines = []
+			for k in data:
+				lines.append(k + " = " + data[k])
+
+			return "\n".join(lines)
+
+
 	loaders = {
 		"yaml":YAMLHandler(),
 		"yml":YAMLHandler(), #yes, two instances for no reason other than cleaner code
@@ -186,26 +252,31 @@ class formats:
 		"ini":INIHandler()
 	}
 
-exampleconfig = Configuration(
-	settings={
-		"Group":{
-			"name":(types.String(),					"Group Name",		"Fellowship of the Ring"),
-			"employer":(types.String(),				"Employer",			"Council of Elrond"),
-			"members":(types.Set(types.String()),	"Members",			["Gandalf","Aragorn","Boromir","Legolas","Gimli","Frodo","Samwise","Peregrin","Meriadoc"])
-		},
-		"Mission":{
-			"start":(types.String(),				"Start Point",		"Rivendell"),
-			"target":(types.String(),				"Target",			"Mount Doom"),
-			"timebudget":(types.Integer(min=4),		"Estimated Days",	360)
+def test():
+	exampleconfig = Configuration(
+		settings={
+			"Group":{
+				"name":(types.String(),					"Group Name",		"Fellowship of the Ring"),
+				"employer":(types.String(),				"Employer",			"Council of Elrond"),
+				"members":(types.Set(types.String()),	"Members",			["Gandalf","Aragorn","Boromir","Legolas","Gimli","Frodo","Samwise","Peregrin","Meriadoc"])
+			},
+			"Mission":{
+				"start":(types.String(),				"Start Point",		"Rivendell"),
+				"target":(types.String(),				"Target",			"Mount Doom"),
+				"timebudget":(types.Integer(min=4),		"Estimated Days",	360)
+			}
 		}
-	}
 
-)
+	)
 
-from bottle import run, get
+	print(exampleconfig.usersettings)
+	from bottle import run, get
 
-@get("/")
-def settingspage():
-	return exampleconfig.html()
+	@get("/")
+	def settingspage():
+		return exampleconfig.html()
 
-run(port=8080,server="waitress")
+	#run(port=8080,server="waitress")
+
+if __name__ == "__main__":
+	test()
